69cf9e6745f513bbe9507488b0c613621f0c8aec
nohatekate <kateiscoding@gmail.com>
2023-04-15 15:45:47 -0400

    dog index view functionality

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 
+
+dogs = [
+  {'name': 'Theo', 'breed': 'mix', 'description': 'black and white wiggle butt', 'age': 5},
+  {'name': 'Evergreen', 'breed': 'German Shepard, Australian Cattle Dog', 'description': 'anxious, but hard working and loyal', 'age': 2},
+]
+
+
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+def dogs_index(request):
+    return render(request, 'dogs/index.html', {
+    'dogs': dogs
+    })
