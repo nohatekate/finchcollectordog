b72680cf8686adc09a52d2084b456a781ade4a61
nohatekate <kateiscoding@gmail.com>
2023-04-15 16:08:24 -0400

    create dog model

--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render
+from .models import Dog
 
 
-dogs = [
-  {'name': 'Theo', 'breed': 'mix', 'description': 'black and white wiggle butt', 'age': 5},
-  {'name': 'Evergreen', 'breed': 'German Shepard, Australian Cattle Dog', 'description': 'anxious, but hard working and loyal', 'age': 2},
-]
 
 
 # Create your views here.
@@ -15,6 +12,7 @@ def about(request):
     return render(request, 'about.html')
 
 def dogs_index(request):
+    dogs = Dog.objects.all()
     return render(request, 'dogs/index.html', {
     'dogs': dogs
     })
