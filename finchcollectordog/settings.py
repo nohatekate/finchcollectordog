014ab5e77a8768d8af58e8c58ac9fc03de6bdfeb
nohatekate <kateiscoding@gmail.com>
2023-04-18 21:02:41 -0500

    hide secret key

--- finchcollectordog/settings.py
+++ finchcollectordog/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_@*0txvq+n$)m83%@50lamfkfrv!!0)%8-y(7oqvngrau+1rit'
+from decouple import config
+
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
